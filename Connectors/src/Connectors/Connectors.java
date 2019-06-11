679aad39b61e5238e47914294cba111f23d9febb
Priyanka <mittumadda@gmail.com>
2019-06-10 22:10:05 -0700

    FIRST COMMIT

---
+++ Connectors/src/Connectors/Connectors.java
@@ -0,0 +1,187 @@
+package Connectors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class Connectors {
+	
+	@Test
+	public void StopDeploymentTestRun()
+	{
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("enable-automation");
+		options.addArguments("--window-size=1920,1080");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-extensions");
+		options.addArguments("--dns-prefetch-disable");
+		options.addArguments("--disable-gpu");
+		driver = new ChromeDriver(options);
+		String url = "http://10.10.200.88/QPlatform/";
+		String HadoopCluster;
+		HadoopCluster="Hdp-13-05-2019 15:21:47";
+		driver.get(url);
+		driver.manage().window().maximize();		
+		driver.manage().deleteAllCookies();
+		
+		
+		
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		WebElement weUser = driver.findElement(By.id("userName"));
+		if(weUser.isDisplayed() && weUser.isEnabled()){
+			weUser.sendKeys("anilk");
+			Assert.assertTrue(true, "User name entered");
+		}
+		else{
+			System.out.println("User name is not displayed");
+			Assert.assertTrue(false, "User name not found");
+			
+		}
+		WebElement wePassword= driver.findElement(By.id("password"));
+		if(wePassword.isDisplayed() && wePassword.isEnabled()){
+			wePassword.sendKeys("Quad1admin")  ;
+			Assert.assertTrue(true, "Password entered");
+		}
+		else{
+			System.out.println("Password is not displayed");
+			Assert.assertTrue(false, "Password not found");
+		}
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		WebElement weLogin = driver.findElement(By.xpath("//button[text()='Login']"));
+		weLogin.click();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		WebElement weconnectors=driver.findElement(By.xpath("//*[@id='manage']/span[1]"));
+		weconnectors.click();
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		WebElement weClusters=driver.findElement(By.id("hadoopClusters"));
+		weClusters.click();
+
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.findElement(By.xpath("//input[@type='search']")).sendKeys(HadoopCluster);
+		WebElement weEdit=driver.findElement(By.xpath("//*[@id='DataTables_Table_0']/tbody/tr[1]/td[6]/a[1]"));
+		weEdit.click();	
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		WebElement weTest=driver.findElement(By.id("hdfsTestBtn"));
+		weTest.click();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		WebElement weHIVEConfig=driver.findElement(By.id("headinghive"));
+		weHIVEConfig.click();
+
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		WebElement weTest1=driver.findElement(By.id("hiveTestBtn"));
+		weTest1.click();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		WebElement weOozieConfig=driver.findElement(By.id("collapseOozie"));
+		weOozieConfig.click();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		WebElement weTest11=driver.findElement(By.id("oozieTestBtn"));
+		weTest11.click();
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		WebElement weYarnConfig=driver.findElement(By.id("headingYarn"));
+		weYarnConfig.click();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		WebElement weTest111=driver.findElement(By.id("yarnTestBtn"));
+		weTest111.click();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		WebElement weSave=driver.findElement(By.id("hadoopClusterDsSaveBtn"));
+		weSave.click();
+
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}}
